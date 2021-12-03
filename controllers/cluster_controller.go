5d37df54ee9572ce7d2816409756ad5122239069
Nick Jüttner <nick@giantswarm.io>
2021-12-03 11:32:51 +0100

    logic implementation

--- controllers/cluster_controller.go
+++ controllers/cluster_controller.go
@@ -19,7 +19,9 @@ package controllers
 import (
 	"context"
 
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -28,7 +30,10 @@ import (
 // ClusterReconciler reconciles a Cluster object
 type ClusterReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
+
+	recorder record.EventRecorder
 }
 
 //+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters,verbs=get;list;watch;create;update;patch;delete
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/giantswarm/cluster-cleaner
 go 1.16
 
 require (
+	github.com/Azure/go-autorest/autorest/adal v0.9.13 // indirect
+	github.com/go-logr/logr v0.4.0 // indirect
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
 	k8s.io/apimachinery v0.22.4
 	k8s.io/client-go v0.22.4
+	sigs.k8s.io/cluster-api v1.0.1 // indirect
 	sigs.k8s.io/controller-runtime v0.10.3
 )

970ef21a12b1a6f21f8cf129e76d9541f5dedd80
Nick Jüttner <nick@giantswarm.io>
2021-12-06 11:58:25 +0100

    dry-run

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	var dryRun bool
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&dryRun, "dry-run", false, "Dry run.")
+	flag.BoolVar(&dryRun, "dry-run", false, "Enable dry-run.")
 	opts := zap.Options{
 		Development: true,
 	}

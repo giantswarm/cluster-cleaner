4fcb83d81369f0ad59ab05cb8c76065a894aaa5a
Laszlo Uveges <laszlo@giantswarm.io>
2026-09-11 15:15:30 +0200

    feat: experimentally change the default TTL to 2h

--- controllers/utility.go
+++ controllers/utility.go
@@ -15,7 +15,7 @@ const (
 	keepUntil             = "keep-until"
 
 	// defaultTTL is the default time to live for a cluster.
-	defaultTTL = 4 * time.Hour
+	defaultTTL = 2 * time.Hour
 
 	// eventDefaultTTL is the default time when we sent a `ClusterMarkedForDeletion` event.
 	eventDefaultTTL = defaultTTL - 1*time.Hour
